765714d1c048b4e69b9f1607ff5d131827387db3
Andrew Woodcock-Squires <andyws@outlook.com>
2014-04-11 14:17:46 +0100

    Kings can no longer move next to each other
    Also setting random seed on app load now.

--- c++/chess/King.cpp
+++ c++/chess/King.cpp
@@ -3,6 +3,7 @@
 #include "king.h"
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 using namespace Chess;
@@ -16,6 +17,7 @@ vector<int> * King::GetPossibleMoves(){
 
 vector<int> * King::GetPossibleMoves(bool includeKing){
 	vector<int> * moves = GetMultiDirectionMoves(1, includeKing);
+	vector<int> * outputMoves = new vector<int>();
 	if(board->TestCastlingMove(colour, false)){
 		moves->push_back((colour == Colour::White ? 6 : 62));
 	}
@@ -23,7 +25,58 @@ vector<int> * King::GetPossibleMoves(bool includeKing){
 	if(board->TestCastlingMove(colour, true)){
 		moves->push_back((colour == Colour::White ? 2 : 58));
 	}
-	return moves;
+
+	for(int m=0; m<moves->size(); m++){
+		int moveRow, moveCol, currentMove;
+		currentMove = moves->at(m);
+		moveRow = currentMove / 8;
+		moveCol = currentMove % 8;
+		bool validMove = true;
+		for(int t=0; t<8; t++){
+			int offsetPosition = 0;
+			switch(t){
+				case 0:
+					offsetPosition = ((moveRow+1)*8)+(moveCol-1);
+					break;
+				case 1:
+					offsetPosition = ((moveRow+1)*8)+moveCol;
+					break;
+				case 2:
+					offsetPosition = ((moveRow+1)*8)+(moveCol+1);
+					break;
+				case 3:
+					offsetPosition = (moveRow*8)+(moveCol-1);
+					break;
+				case 4:
+					offsetPosition = (moveRow*8)+(moveCol+1);
+					break;
+				case 5:
+					offsetPosition = ((moveRow-1)*8)+(moveCol-1);
+					break;
+				case 6:
+					offsetPosition = ((moveRow-1)*8)+moveCol;
+					break;
+				case 7:
+					offsetPosition = ((moveRow-1)*8)+(moveCol+1);
+					break;
+			}
+
+			if(offsetPosition > 0 && offsetPosition < 64){
+				Piece * piece = board->PieceAtPosition(offsetPosition);
+				if(piece != nullptr && piece->GetColour() != colour && dynamic_cast<King*>(piece) != nullptr){
+					validMove = false;
+					break;
+				}
+			}
+		}
+
+		if(validMove){
+			outputMoves->push_back(moves->at(m));
+		}
+	}
+	delete moves;
+
+	return outputMoves;
 }
 
 Piece * King::CopyTo(Board * _board){
--- c++/chess/main.cpp
+++ c++/chess/main.cpp
@@ -4,6 +4,8 @@
 #include "ConsoleChessRenderer.h"
 #include "game.h"
 #include "aitypejarvis.h"
+#include <ctime>
+
 
 using namespace std;
 using namespace Chess;
@@ -11,6 +13,7 @@ using namespace Chess::Renderer;
 
 int main()
 {
+	srand(time(NULL));
 	BaseRenderer * renderer = new ConsoleChessRenderer();
 	Game game = Game(renderer);
 	game.AddAIPlayer(new AIType_Jarvis());
